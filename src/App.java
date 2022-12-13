a3f77a00acba963849516b2abaa22beb19093f9e
Samekah <lucidareverie@outlook.com>
2022-12-13 23:57:52 +0000

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    /* This code will allow a user to run 4 basic apps, these being
+     *  [1] Magic number 5
+     *  [2] Counting application
+     *  [3] Cm to Ft and inches conversion
+     *  [4] Fibonacci sequence
+     *  [5] Cylinder volume calculator
+    */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, welcome to my Java refresher app! \nThis code will allow a user to run 5 basic apps, these being:");
+        System.out.println("[1] Magic number 5, \n[2] Counting application, \n[3] Cm to Ft and inches conversion, \n[4] Fibonacci sequence, \n[5] Cylinder volume calculator\n");
+        System.out.println("Each program will have their own instructions on how they can be used and an explination of what they can do!");
+    }
+}
