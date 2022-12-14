80d8a4979bce406c842bf6307b6a03e79d017d8a
Samekah <lucidareverie@outlook.com>
2022-12-14 00:25:44 +0000

    added boilerplate for Magic Number Five class

---
+++ src/MagicNumberFive.java
@@ -0,0 +1,19 @@
+/**
+ * MagicNumberFive
+ */
+public class MagicNumberFive {
+
+    private int startnum;
+
+    public MagicNumberFive(int startNum){
+        this.startnum = startNum;
+    }
+
+    public void appInfo(){
+
+    }
+
+    public int beginMagic(){
+        return 0;
+    }
+}
