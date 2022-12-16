7e244e677c11cd47d4298b56568c0cc56e7a8a15
Samekah <lucidareverie@outlook.com>
2022-12-16 00:54:41 +0000

    added user input to main menu and added exit option

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+import javax.xml.ws.Response;
+
 public class App {
     /* This code will allow a user to run 4 basic apps, these being
      *  [1] Magic number 5
@@ -7,40 +11,45 @@ public class App {
      *  [5] Cylinder volume calculator
     */
     public static void main(String[] args) throws Exception {
-        String response = "Exit";
-       
-        while(!response.equals("Exit") && !response.equals("exit")){
-            System.err.println(!response.equals("Exit"));
+        String response = "";
+        Scanner input = new Scanner(System.in);
+
+        while(true){
             System.out.println("Hello, welcome to my Java refresher app! \nThis code will allow a user to run 5 basic apps, these being:");
-            System.out.println("[1] Magic number 5, \n[2] Counting application, \n[3] Cm to Ft and inches, \n[4] Fibonacci sequence, \n[5] Cylinder volume calculator\n");
+            System.out.println("\n[1] Magic number 5, \n[2] Counting application, \n[3] Cm to Ft and inches, \n[4] Fibonacci sequence, \n[5] Cylinder volume calculator\n");
             System.out.println("Each program will have their own instructions on how they can be used and an explination of what they can do!");
             System.out.println("Please select an option to run, either by name or number");
 
             //section to retrieve input
-
-            if(response.equals("Magic number 5") || response=="1"){
-                System.out.println("Magic Number 5 selected");
+            response = input.nextLine();
+            if(response.equals("Magic number 5") || response.equals("1")){
+                System.out.println("\nMagic Number 5 selected");
                
             }
-           else if(response.equals("Counting application") || response=="2"){
-                System.out.println("Counting application selected");
+           else if(response.equals("Counting application") || response.equals("2")){
+                System.out.println("\nCounting application selected");
 
             }    
-           else if(response.equals("Cm to Ft and inches") || response=="3"){
-                System.out.println("Cm to Ft and inches conversion selected");
+           else if(response.equals("Cm to Ft and inches") || response.equals("3")){
+                System.out.println("\nCm to Ft and inches conversion selected");
          
             }
-            else if(response.equals("Fibonacci sequence") || response=="4"){
-                System.out.println("Fibonacci sequence selected");
+            else if(response.equals("Fibonacci sequence") || response.equals("4")){
+                System.out.println("\nFibonacci sequence selected");
             }
-            else if(response.equals("Cylinder volume calculator") || response=="5"){
-                System.out.println("Cylinder volume calculator selected");
+            else if(response.equals("Cylinder volume calculator") || response.equals("5")){
+                System.out.println("\nCylinder volume calculator selected");
+            }
+            else if(response.equals("Exit") || response.equals("exit")){
+                System.out.println("\nThank you for using this appication!");
+                input.close();
+                break;
             }
             else{
-                System.out.println("Unfortunately, " + response + " is not a valid option!");
+                System.out.printf("\n\nUnfortunately, %s is not a valid option!\n", response);
             }
        }
-       System.out.println("Thank you for using this appication!");
+       
         
         //select option
         //give explination
