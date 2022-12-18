193cf1f7408c2b817d45816dcb71f1379bbf5180
Samekah <lucidareverie@outlook.com>
2022-12-18 00:12:33 +0000

    added fibonacci, volume calculator and CMConversion files

---
+++ src/CmConversion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CmConversion implements Applications{
+
+    @Override
+    public void subMenu(Scanner magicInput) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void demo() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stepThrough() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+}
